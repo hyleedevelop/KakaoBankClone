d542bc5d25baba81905d73f20c1dc245e66c240c
Hoyeon Lee <hyleedevelop@gmail.com>
2023-08-27 23:08:41 +0900

    v0.2.10
    - 푸시 알림을 Firebase Messaging에서 받을 수 있도록 구현 (테스트 단계까지 완료)

--- KakaoBankClone/AppDelegate.swift
+++ KakaoBankClone/AppDelegate.swift
@@ -8,6 +8,8 @@
 import UIKit
 import FirebaseCore
 import FirebaseAuth
+import FirebaseMessaging
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -21,23 +23,66 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // 앱 실행 후 계좌 화면의 테이블뷰 셀 애니메이션이 최초 1회만 시작되도록 설정
         UserDefaults.standard.showCellAnimation = true
         
+        // 푸시 알림 관련 설정
+        Messaging.messaging().delegate = self
+        
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { granted, _ in
+            if granted {
+                print("알림 등록이 완료되었습니다.")
+            }
+        }
+        application.registerForRemoteNotifications()
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 
+    }
+    
+    // registerForRemoteNotifications() 메서드 성공 시 호출
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+//        // 수신된 deviceToken을 String으로 변환하기
+//        // 푸시 알림을 보낼 때 서버는 이 토큰을 “주소”로 사용하여 올바른 목적지 기기에 전달함
+//        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+//        let token = tokenParts.joined()
+//        print("Device Token: \(token)")
+        
+        // FirebaseMessaging - 토큰
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    // registerForRemoteNotifications() 메서드 실패 시 호출
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register: \(error)")
+    }
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    // foreground에서도 알림이 보이도록 설정
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+}
+
+extension AppDelegate: MessagingDelegate {
+
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        // 원래는 서버로 다시 fcm 토큰을 보내줘야 하지만 서버가 없기 때문에 token을 출력
+        if let fcmToken = fcmToken {
+            print("FCM Token: \(fcmToken)")
+        }
+    }
+    
+}
